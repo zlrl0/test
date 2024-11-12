d8a96305f35bf2303a31bf7f4a71fdeb9b336a19
zlrl0 <jungjieun2002@gmail.com>
2024-11-12 14:34:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
+for i in range(5):
+  print("Hello World")
